44cfce7ce45622f83e90aa05552ed24573643cd3
sayanta2002 <sayantabiswas20022002@gmail.com>
2023-02-20 00:17:27 +0530

    Committed

--- stringadvanced 1.py
+++ stringadvanced 1.py
@@ -1,4 +1,5 @@
 # Strings are incommutable but supports Concatenation
+# Strings once assigned can't be changed by element
 
 # Python3 program to show that
 # string cannot be changed
@@ -13,3 +14,36 @@ print(a)
 #print(a)  # causes error
 
 print(b) #Supported little concatenation
+print(id(a)) #tells the identity where the string is stored in memory
+
+#3 ways to create strings
+
+# Python3 program to create
+# strings in three different
+# ways and concatenate them.
+
+# string with single quotes
+a = 'Geeks'
+
+# string with double quotes
+b = "for"
+
+# string with triple quotes
+c = '''Geeks a portal 
+for geeks'''
+
+d = '''He said, "I'm fine."'''
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Concatenation of strings created
+# using different quotes
+print(a + b + c)
+
+print('"Hey so happy to be here"') #Use " ' ' " or ' " " '  Configuration for showing Single or Double Quotes
+
+# use of mix quotes
+print('Getting Geeky, "Loving it"')
