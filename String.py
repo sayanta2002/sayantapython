04d29e7a9045f98297434fb4ce3ce7cf512e755b
sayanta2002 <sayantabiswas20022002@gmail.com>
2023-02-19 20:12:43 +0530

    Committed

---
+++ String.py
@@ -0,0 +1,7 @@
+name = "Sayanta Biswas" #Strigs can be written using Single or Double quotes
+u = len(name)           #len function tells the length of the strings including white spaces
+print(u)                #Answer 14 (Including Space ) (Starting from 0 to 13)
+
+print(name[0])          #THat [] is like array in C/C++ . The 0th element is 'S'
+print(name[1])
+                        #print(u[0]) can't be available because [] dopes not work on Data Types other than Strings
