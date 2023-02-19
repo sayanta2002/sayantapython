1fa46d63067e39d08128dbcf8f787b47f2a0e04a
sayanta2002 <sayantabiswas20022002@gmail.com>
2023-02-19 18:36:59 +0530

    Committed

---
+++ Refactoring.py
@@ -0,0 +1,10 @@
+p = 69
+
+print (p)
+print (p)
+print (p)
+print (p)
+
+#If I want to change i to something else then Pycharm ; Right click on it
+#-- click Refactor -- Click rename -- set the new -- refactor
+#Make sure that this applies to current file (Tick the box)
